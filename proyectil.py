08f8537da13de556519789d4fe192cb92e366e79
Gastón Hugo <78811877+ghugo114@users.noreply.github.com>
2025-10-31 22:13:25 -0300

    Update proyectil.py

--- proyectil.py
+++ proyectil.py
@@ -82,5 +82,99 @@ xs=numpy.zeros(2)
 vs=numpy.zeros(2)
 e=numpy.zeros(3)
 xa = numpy.zeros(int((N/10)+1))
-                                                                                                                                                                                                                                                                                                                             46,9     Comienzo
+ya = numpy.zeros(int((N/10)+1))
+
+for i in range(0,1,1):
+    xs[i]=0.0
+tr=theta*pi/180.
+vs[0]=v0*cos(tr)
+vs[1]=v0*sin(tr)
+i=0
+e=energy(xs,vs,sdt,g)
+fout1.write('%f\t%f\t%f\t%f\t%f\n' % (sdt,xs[0],xs[1],vs[0],vs[1]))
+fout2.write('%f\t%f\t%f\t%f\n' % (sdt,e[0],e[1],e[2]))
+D=(2.*(v0**2)*cos(tr)*sin(tr)/g)
+H=((v0 * sin(tr))**2)/(2.*g)
+
+j=0
+xa[0]=xs[0]
+ya[0]=xs[1]
+#------------------Loop--------------------------------------
+while (sdt < T):
+    xs,vs=verlet(xs,vs,sdt,g,ga)
+    sdt=sdt+(tstep)
+    i=i+1
+    if ((xs[1]<h_m)&(xs[1]>0.0)&(xs[0]>(d_m-a_m))&(xs[0]<(d_m+a_m))):
+        vs[0]=-vs[0]
+    elif xs[1]<=0:
+        vs[1]=-vs[1]
+    elif xs[0]<=0:
+        vs[0]=-vs[0]
+    elif xs[0]>=D:
+        vs[0]=-vs[0]
+    elif xs[1]>=(H+5.0):
+        vs[1]=-vs[1]
+    else:
+        pass
+    if (i==10):
+        j=j+1
+        xa[j]=xs[0]
+        ya[j]=xs[1]
+        e=energy(xs,vs,sdt,g)
+        fout1.write('%f\t%f\t%f\t%f\t%f\n' % (sdt,xs[0],xs[1],vs[0],vs[1]))
+        fout2.write('%f\t%f\t%f\t%f\n' % (sdt,e[0],e[1],e[2]))
+
+        i=0
+    else:
+        pass
+#----------------------------------------------------------------
+D=((v0**2)*cos(tr)*sin(tr)/g)
+H=((v0 * sin(tr))**2)/(2.*g)
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(autoscale_on=False, xlim=(0, 2.*D), ylim=(-10, H+5.))
+ax.set_aspect('equal')
+ax.grid()
+
+line, = ax.plot([], [], 'o-', lw=2)
+trace, = ax.plot([], [], '.-', lw=1, ms=1)
+wall, = ax.plot([], [], '.-', lw=2)
+time_template = 'time = %.1fs'
+time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)             
+plt.grid(True)
+plt.title("Movimiento en 2 dimensiones: Proyectiles")
+plt.xlabel("Coordenada x")
+plt.ylabel("Coordenada y")
+
+
+
+def animate(i):
+    thisx = [xa[i]]
+    thisy = [ya[i]]
+    wallx = [[d_m,d_m]]
+    wally = [[0,h_m]]
+    history_x = xa[:i]
+    history_y = ya[:i]
+
+    line.set_data(thisx, thisy)
+    trace.set_data(history_x, history_y)
+    wall.set_data(wallx,wally)
+    time_text.set_text(time_template % ((i*10)*dt))
+    return line, wall, trace, time_text
+
+ani = animation.FuncAnimation(fig, animate, len(xa), interval=dt*10, blit=False)
+fout1.close()
+fout2.close()
+
+
+plt.figure(figsize=(4, 3), dpi=100)
+plt.text(0.1,1.0,r'Coordenadas del vector r:',fontsize=11)
+plt.text(0.1,0.9,r' x (MRU): $x(t)=x_0+v_0 \cos(\theta) t$',fontsize=11)
+plt.text(0.1,0.8,r' y (MRUV): $y(t)=y_0+v_0 \sin(\theta) t-\frac{g t^2}{2}$',fontsize=11)
+plt.text(0.1,0.7,r'Coordenadas del vector v:',fontsize=11)
+plt.text(0.1,0.6,r'$v_x(t)=v_0 \cos(\theta) $',fontsize=11)
+plt.text(0.1,0.5,r'$v_y(t)=v_0 \sin(\theta) - g t$',fontsize=11)
+
+plt.axis('off')
+plt.show()
+                                                                                                                                                                                                                                                                                                         104,9         65%
 
