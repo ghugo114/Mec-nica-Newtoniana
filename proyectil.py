5d6c2554ad197b4fb0a011f9d26e7b8687ce7c88
Gastón Hugo <78811877+ghugo114@users.noreply.github.com>
2025-11-01 19:04:45 -0300

    Nueva versión
    Primero muestra material teórico, luego animación.

--- proyectil.py
+++ proyectil.py
@@ -5,18 +5,18 @@ import matplotlib.animation as animation
 from numpy import *
 #---------------------------------------------------------------
 #----------Parametros-------------------------------------------
-tstep=0.005
+tstep=0.001
 dt=tstep
 pi=2.0*arcsin(1.0)
 sdt=0.0
 #--------------- Funcion Fuerza---------------------------------
 def f(xs,vs,t,g,ga):
-
+    
     fx=-ga*vs[0]
     fy=-g-(ga*vs[1])
     return fx,fy
 #--------------------------------------------------------------
-def euler(x,v,t,w02,g,a,w):
+def euler(x,v,t,w02,g,a,w): 
     v_h=v+(f(x,v,t,w02,g,a,w)*(tstep))
     x_h=x+v*(tstep)
     return x_h,v_h
@@ -36,29 +36,57 @@ def energy(xs,vs,t,g):
     ec=(vs[0]**2+vs[1]**2)/2.
     em=ep+ec
     return ep,ec,em
+
+
 #--------------------------------------------------------------------
+def rebotes(xs,vs,d_m,h_m,a_m):
+    if ((xs[1]<h_m)&(xs[1]>0.0)&(xs[0]>(d_m-a_m))&(xs[0]<(d_m+a_m))):
+        vs[0]=-vs[0]
+    elif xs[1]<0.01:
+        vs[1]=-vs[1]
+    elif xs[0]<=0.01:
+        vs[0]=-vs[0]
+    elif xs[0]>=D-0.01:
+        vs[0]=-vs[0]
+    elif xs[1]>=(H+5.0):
+        vs[1]=-vs[1]
+    else:
+        pass
+    return xs,vs
+
+
 #if len(sys.argv) < 6:
- #   sys.exit("Usage: python3 proyectil.py g gamma theta v0 T")
+ #   sys.exit("Usage: python3 proyectil.py g gamma theta v0 T obs_x obs_y")
 #coef = input("Ingrese g,gamma,angulo,velocidad,tmax: ej 9.8,0.1,45.0,23.0,10.0 \n").split(',')
 #coef = numpy.array([float(a) for a in coef])
 #muro = input("Ingrese distancia a la que se encuentra y altura del obstaculo: ej 10.0,5.0 (a 10 m de dist, 5 m altura)\n").split(',')
 #muro = numpy.array([float(b) for b in muro])
-print("Movimiento en dos dimensiones: Proyectiles bajo efecto de la gravedad\n")
-print("con efecto de rozamiento con el aire, donde F es proporcional a velocidad\n")
-print("y de signo opuesto cuyo modulo se calcula F_roz= -ga v, siendo F_roz y v vectores\n")
-print("A continuacion ingrese g, ga (0 para no rozamiento), angulo inicial, velocidad inicial,\n")
-print("y tiempo total cuando lea: Ingrese g,gamma,angulo,velocidad,tmax: ej 9.8,0.1,45.0,23.0,10.0 \n")
-print("Luego ingrese la distancia a la que se encuentra un obstaculo y su altura\n")
-print("Apareceran dos ventanas, una con el movimiento y trayectoria del proyectil\n")
-print("Otra con las ecuaciones de movimiento para el caso sin rozamiento\n")
+
+plt.figure(figsize=(15, 15), dpi=100)
+plt.text(0.0,1.0,r'Movimiento en dos dimensiones: Proyectiles bajo efecto de la gravedad', fontsize=11)
+plt.text(0.0,0.9,r'Se incluye el efecto de la fuerza (viscosa) de rozamiento con el aire proporcional a velocidad y de signo opuesto.',fontsize=11)
+plt.text(0.0,0.8,r'El módulo de esta fuerza se calcula:',fontsize=11)
+plt.text(0.0,0.7,r'$\vec{F}_{roz}= -b \vec{v} $ para lo cual se solicita al usuario el parámetro $\gamma=\frac{b}{m}$' ,fontsize=11)
+plt.text(0.0,0.6,r' Las ecuaciones solución del movimiento sin efecto del aire son:',fontsize=11) 
+plt.text(0.1,0.5,r' x (MRU): $x(t)=x_0+v_0 \cos(\theta) t$',fontsize=11)
+plt.text(0.1,0.4,r' y (MRUV): $y(t)=y_0+v_0 \sin(\theta) t-\frac{g t^2}{2}$',fontsize=11)
+plt.text(0.1,0.3,r'Coordenadas del vector v:',fontsize=11)
+plt.text(0.1,0.2,r'$v_x(t)=v_0 \cos(\theta) $',fontsize=11)
+plt.text(0.1,0.1,r'$v_y(t)=v_0 \sin(\theta) - g t$',fontsize=11)
+
+plt.axis('off')
+plt.show()
 print("Programa desarrollado por Lic. Gastón Hugo, clases particulares de Física y Matemática\n")
 print("Celular: 099750940\n")
 print("Email: gaston.hugo@gmail.com\n")
-print("Material disponible en https://gastonhugo.wixsite.com/cursos\n")
+print("Material en pdf disponible en: https://gastonhugo.wixsite.com/cursos\n")
+print("                               https://sites.google.com/view/gastonhugocomputaconal/home\n")
+      
 #------------------Comienza el programa--------------------------------
 print("\n")
 print("Comienza el programa que simula el movimiento de un proyectil con\n")
-print("agregado de un obstaculo contra el que el objeto rebota si se encuentra con el\n")
+print("agregado de un obstaculo contra el que el objeto rebota. Además se asume \n")
+print("que a altura cero se encuentra el piso y que los bordes del gráfico simulan paredes contra las que el objeto también rebota.\n")
 coef = input("Ingrese g,gamma,angulo,velocidad,tmax: ej 9.8,0.1,45.0,23.0,10.0 \n").split(',')
 coef = numpy.array([float(a) for a in coef])
 muro = input("Ingrese distancia a la que se encuentra y altura del obstaculo: ej 10.0,5.0 (a 10 m de dist, 5 m altura)\n").split(',')
@@ -66,17 +94,17 @@ muro = numpy.array([float(b) for b in muro])
 
 
 g=coef[0]#float(sys.argv[1])
-ga=coef[1]#float(sys.argv[2])
-theta=coef[2]#float(sys.argv[3])
-v0=coef[3]#float(sys.argv[4])
-T=coef[4]#float(sys.argv[5])
+ga=coef[1]#sys.argv[2])
+theta=coef[2]#sys.argv[3])
+v0=coef[3]#sys.argv[4])
+T=coef[4]#sys.argv[5])
 N=int(T/tstep)
 #fpar=open('param_in.dat','w')
 fout1=open('datos.dat','w')
 fout2=open('energ.dat','w')
 
-h_m=muro[1]#5.
-d_m=muro[0]#10.
+d_m=float(muro[0])#sys.argv[6])
+h_m=float(muro[1])#sys.argv[7])
 a_m=0.05
 xs=numpy.zeros(2)
 vs=numpy.zeros(2)
@@ -85,7 +113,7 @@ xa = numpy.zeros(int((N/10)+1))
 ya = numpy.zeros(int((N/10)+1))
 
 for i in range(0,1,1):
-    xs[i]=0.0
+    xs[i]=0.01
 tr=theta*pi/180.
 vs[0]=v0*cos(tr)
 vs[1]=v0*sin(tr)
@@ -104,18 +132,7 @@ while (sdt < T):
     xs,vs=verlet(xs,vs,sdt,g,ga)
     sdt=sdt+(tstep)
     i=i+1
-    if ((xs[1]<h_m)&(xs[1]>0.0)&(xs[0]>(d_m-a_m))&(xs[0]<(d_m+a_m))):
-        vs[0]=-vs[0]
-    elif xs[1]<=0:
-        vs[1]=-vs[1]
-    elif xs[0]<=0:
-        vs[0]=-vs[0]
-    elif xs[0]>=D:
-        vs[0]=-vs[0]
-    elif xs[1]>=(H+5.0):
-        vs[1]=-vs[1]
-    else:
-        pass
+    rebotes(xs,vs,d_m,h_m,a_m)
     if (i==10):
         j=j+1
         xa[j]=xs[0]
@@ -139,7 +156,7 @@ line, = ax.plot([], [], 'o-', lw=2)
 trace, = ax.plot([], [], '.-', lw=1, ms=1)
 wall, = ax.plot([], [], '.-', lw=2)
 time_template = 'time = %.1fs'
-time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)             
+time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
 plt.grid(True)
 plt.title("Movimiento en 2 dimensiones: Proyectiles")
 plt.xlabel("Coordenada x")
@@ -161,20 +178,35 @@ def animate(i):
     time_text.set_text(time_template % ((i*10)*dt))
     return line, wall, trace, time_text
 
-ani = animation.FuncAnimation(fig, animate, len(xa), interval=dt*10, blit=False)
+class PauseAnimation:
+    def __init__(self):
+        #fig, ax = plt.subplots()
+        ax.set_title('Click to pause/resume the animation')
+        self.animation = animation.FuncAnimation(
+            fig, animate, len(xa), interval=dt*100, blit=True)
+        self.paused = False
+
+        fig.canvas.mpl_connect('button_press_event', self.toggle_pause)
+
+    def toggle_pause(self, *args, **kwargs):
+        if self.paused:
+            self.animation.resume()
+        else:
+            self.animation.pause()
+        self.paused = not self.paused
+
+#    def update(self, i):
+ #       self.n0 += i / 100 % 5
+  #      self.p.set_ydata(self.n0 % 20)
+   #     return (self.p,)
+
+
+pa = PauseAnimation()
+plt.show()
+
+
+#ani = animation.FuncAnimation(fig, animate, len(xa), interval=dt*10, blit=False)
+
 fout1.close()
 fout2.close()
-
-
-plt.figure(figsize=(4, 3), dpi=100)
-plt.text(0.1,1.0,r'Coordenadas del vector r:',fontsize=11)
-plt.text(0.1,0.9,r' x (MRU): $x(t)=x_0+v_0 \cos(\theta) t$',fontsize=11)
-plt.text(0.1,0.8,r' y (MRUV): $y(t)=y_0+v_0 \sin(\theta) t-\frac{g t^2}{2}$',fontsize=11)
-plt.text(0.1,0.7,r'Coordenadas del vector v:',fontsize=11)
-plt.text(0.1,0.6,r'$v_x(t)=v_0 \cos(\theta) $',fontsize=11)
-plt.text(0.1,0.5,r'$v_y(t)=v_0 \sin(\theta) - g t$',fontsize=11)
-
-plt.axis('off')
-plt.show()
-                                                                                                                                                                                                                                                                                                         104,9         65%
 
